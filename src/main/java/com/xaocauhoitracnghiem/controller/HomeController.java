2080e53d181a80e54d55483f68cb0f8bf4db1d2e
tuanlong23 <105482880+tuanlong23@users.noreply.github.com>
2024-07-02 15:23:24 +0700

    Update HomeController.java

--- src/main/java/com/xaocauhoitracnghiem/controller/HomeController.java
+++ src/main/java/com/xaocauhoitracnghiem/controller/HomeController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher rd = req.getRequestDispatcher("/views/home.jsp");
+		RequestDispatcher rd = req.getRequestDispatcher("/views/web/home.jsp");
 		rd.forward(req, resp);
 	}
 	
