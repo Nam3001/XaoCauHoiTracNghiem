a7da00753ca88e5b116206b603de1cb9a62fb398
Nam3001 <nguyenvn30012003@gmail.com>
2024-07-17 10:08:29 +0700

    fix utf-8 encode in MathMLToEquation

--- src/main/java/com/xaocauhoitracnghiem/utils/MathMLToEquation.java
+++ src/main/java/com/xaocauhoitracnghiem/utils/MathMLToEquation.java
@@ -121,8 +121,8 @@ public class MathMLToEquation {
 					} else if (xmlcursor.getName().getLocalPart().equalsIgnoreCase("oMath")) {
 						// append contentString to desParagraph
 //						if (desParagraph.getRuns().size() == 0) {
-						if (contentString.matches("^(C�u|Question)\\s\\d+[:.].*") && questionLabelCount == 0) {
-							contentString = contentString.replaceAll("^(C�u|Question)\\s\\d+[:.]", "").trim();
+						if (contentString.matches("^(Câu|Question)\\s\\d+[:.].*") && questionLabelCount == 0) {
+							contentString = contentString.replaceAll("^(Câu|Question)\\s\\d+[:.]", "").trim();
 							questionLabelCount++;
 						} else if (contentString.matches("^[A-Z]\\..*") && answerLabelCount == 0) {
 							contentString = contentString.replaceAll("^[A-Z]\\.", "").trim();
@@ -144,8 +144,8 @@ public class MathMLToEquation {
 							&& xmlcursor.getName().getLocalPart().equalsIgnoreCase("drawing")) {
 						// append contentString to desParagraph
 //						if (desParagraph.getRuns().size() == 0) {
-						if (contentString.matches("^(C�u|Question)\\s\\d+[:.].*") && questionLabelCount == 0) {
-							contentString = contentString.replaceAll("^(C�u|Question)\\s\\d+[:.]", "").trim();
+						if (contentString.matches("^(Câu|Question)\\s\\d+[:.].*") && questionLabelCount == 0) {
+							contentString = contentString.replaceAll("^(Câu|Question)\\s\\d+[:.]", "").trim();
 							questionLabelCount++;
 						} else if (contentString.matches("^[A-Z]\\..*") && answerLabelCount == 0) {
 							contentString = contentString.replaceAll("^[A-Z]\\.", "").trim();
